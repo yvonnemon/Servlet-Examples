811cb0bc6b52f6a4fdd1e4b875f83daf7ec24157
xaxoxavi <xtorrens@esliceu.com>
2016-11-02 20:40:46 +0100

    Primer commit

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,82 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Xavi Torrens
+ */
+public class HelloServlet extends HttpServlet {
+
+    private String message;
+
+    public void init() throws ServletException
+    {
+        // Do required initialization
+        message = "Hello World";
+        System.out.println("Servlet " + HelloServlet.class.getName() + " OK");
+
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String first_name = request.getParameter("first_name");
+        String last_name = request.getParameter("last_name");
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String title = "Using POST Method to Read Form Data";
+        String docType =
+                "<!doctype html public \"-//w3c//dtd html 4.0 " +
+                        "transitional//en\">\n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "  <li><b>First Name</b>: "
+                + first_name + "\n" +
+                "  <li><b>Last Name</b>: "
+                + last_name + "\n" +
+                "  <li><b>Fisica</b>: "
+                + request.getParameter("fisica") + "</li>\n" +
+                "  <li><b>Mates</b>: "
+                + request.getParameter("mates") + "</li>\n" +
+                "  <li><b>Quimica</b>: "
+                + request.getParameter("quimica") + "</li>\n" +
+                "</ul>\n" +
+                "</body></html>");
+
+    }
+
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+
+
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        String nom = request.getParameter("nom");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + " " + nom + "</h1>");
+    }
+
+    public void destroy()
+    {
+        System.out.println("Salimos!!!");
+    }
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<html>
+<body>
+<form action="/HelloUrl" method="POST">
+    First Name: <input type="text" name="first_name">
+    <br />
+    Last Name: <input type="text" name="last_name" />
+
+    <input type="checkbox" name="mates" checked="checked" /> Maths
+    <input type="checkbox" name="fisica"  /> Physics
+    <input type="checkbox" name="quimica" checked="checked" /> Quimics
+
+    <input type="submit" value="Submit" />
+</form>
+</body>
+</html>
